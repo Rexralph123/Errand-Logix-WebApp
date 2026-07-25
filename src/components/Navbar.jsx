829ad158e58fa5c8259486180bc15afa850ee414
Rexralph123 <michugonna917@gmail.com>
2026-07-25 19:41:59 +0100

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
     import { Link } from "react-router-dom";
-    import logo from "../assets/images/Errand-logix-logo.png"; // adjust relative path as needed
+    import logo from "./Hero"; // adjust relative path as needed
 
     const NAV_LINKS = [
     { path: "/", label: "Home" },
